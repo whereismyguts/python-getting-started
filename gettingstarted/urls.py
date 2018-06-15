7e46e2e01709df5a2f8d84cca10809f3a3020ee0
karmanov anton <@KARMANOV>
2018-06-15 17:25:19 +0300

    fix

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -13,6 +13,6 @@ import hello.views
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
-    url(r'^item', hello.views.item, name='item'),
+    # url(r'^item', hello.views.item, name='item'),
     path('admin/', admin.site.urls),
 ]
